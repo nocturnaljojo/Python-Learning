524a7e4ed10ab177f550003eb871736b87d58de9
nocturnaljojo <107346524+nocturnaljojo@users.noreply.github.com>
2022-06-13 20:51:29 +1000

    Create Exercise 2 - BMI Calculator.py

---
+++ Day 2/Exercise 2 - BMI Calculator.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+result =int(weight)/(float(height)*float(height))
+bmi = int(result)
+print(bmi)
+
+# Example Input
+# weight = 80
+# height = 1.75
+
+# Example Output
+# 80 ÷ (1.75 x 1.75) = 26.122448979591837
+# 26
