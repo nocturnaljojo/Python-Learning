2cbab19bd9b3bc0d152b53a95532dcf23faf88b5
nocturnaljojo <107346524+nocturnaljojo@users.noreply.github.com>
2022-06-13 20:08:10 +1000

    Create Exercise 2- input Functions.py

---
+++ day1/Exercise 2- input Functions.py
@@ -0,0 +1,6 @@
+#Write your code below this line 👇
+print(len ( input( "What is your name?") ) )
+
+
+#When you hit run, the output will ask"What is your name?
+# After you type your name - the output will count how many letters you have in your name 
