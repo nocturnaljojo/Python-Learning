4ea46c9a1f6141e01b18050335980549c75e9c53
nocturnaljojo <107346524+nocturnaljojo@users.noreply.github.com>
2022-06-13 20:24:22 +1000

    Create Exercise 1 - Data Types.py

---
+++ Day 2/Exercise 1 - Data Types.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+print(int(first_digit) + int(second_digit))
+
+# Example Input
+# 39
+# Example Output
+# 3 + 9 = 12
+# 12
