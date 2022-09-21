fc89c7743f66b891e8115a3aec3ae3d24d1a581b
Jovilisi D <107346524+nocturnaljojo@users.noreply.github.com>
2022-09-22 08:32:04 +1000

    Create for_loops_and_range_intro.py

---
+++ Day 5/for_loops_and_range_intro.py
@@ -0,0 +1,6 @@
+
+# This exercise will add the each value of "number" in a range from 1 to 100. The answer should be 5050 the output
+total = 0
+for number in range(1,101):
+  total = total + number
+print(total)
